7a2000594ff4b76880ca0024fc197f1dc1a8333b
Yonle <yonle@lecturify.net>
2024-04-18 23:38:43 +0700

    bugfix
    Signed-off-by: Yonle <yonle@lecturify.net>

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 "use strict";
 const { listen, port, urlprefix, fastcgi } = require("./config");
 const express = require("express");
+const fs = require("fs");
 const feed = require("./feed.js");
+const styleCSS = fs.readFileSync(__dirname + "/public/style.css", "utf8");
 
 const app = express();
 
@@ -9,14 +11,12 @@ app.set("views", __dirname + "/local/views");
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + "/local/public"));
-app.use(express.static(__dirname + "/public"));
-
-app.get(urlprefix || "/", (req, res) => {
+app.get(urlprefix + "style.css", (req, res) => res.header("Content-Type", "text/css; charset=UTF-8").send(styleCSS));
+app.get(urlprefix, (req, res) => {
   const profile = feed.getProfile();
   const notes = feed.getNotes();
 
-  if (!profile || notes) return res.status(500).header("Content-Type", "text/plain").send(`Sorry. This page is not ready. Please try again later.\n\nAlternatively, Open the following npub via your nostr client:\n${feed.npub}`);
+  if (!profile || !notes) return res.status(500).header("Content-Type", "text/plain").send(`Sorry. This page is not ready. Please try again later.\n\nAlternatively, Open the following npub via your nostr client:\n${feed.npub}`);
 
   res.render("feed.ejs", {
     profile, notes, npub: feed.npub
